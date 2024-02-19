50ee025d3079d012c57afaa1ce824165d5103088
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2024-02-19 18:14:51 -0300

    Scene modifications; tags and layering; background board view

---
+++ Assets/Game/Scripts/Piece.cs
@@ -0,0 +1,22 @@
+
+//using UnityEngine;
+
+//namespace Game.Board.Gems
+//{
+//    [System.Serializable]
+//    public class Piece
+//    {
+//        [SerializeField] private Gem gem;
+//        public Transform transform;
+//        [field: SerializeField] public bool IsSpecial { get; private set; }
+
+//        public Piece(Gem gem, Transform transform)
+//        {
+//            this.gem = gem;
+//            this.transform = transform;
+//        }
+
+//    }
+
+
+//}
