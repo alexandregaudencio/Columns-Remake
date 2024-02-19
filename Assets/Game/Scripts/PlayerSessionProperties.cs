5e73efa4ee99c2a121c3355306705674cc006194
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2024-02-19 18:13:58 -0300

    session properties and player input test

---
+++ Assets/Game/Scripts/PlayerSessionProperties.cs
@@ -0,0 +1,52 @@
+using Game.Board.Gems;
+using System;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerSessionProperties : MonoBehaviour
+    {
+
+        [SerializeField] private int currentSequenceIndex = -1;
+        public event Action<int> SequenceIndexUpdate;
+        public Sequence CurrentSequence
+        {
+            get
+            {
+                return GemSequenceProvider.Instance.FindSequence(currentSequenceIndex);
+            }
+        }
+
+        public Sequence NextSequence
+        {
+            get
+            {
+                return GemSequenceProvider.Instance.FindSequence(currentSequenceIndex + 1);
+            }
+        }
+
+
+        private void Start()
+        {
+            SetNextSequenceIndex();
+        }
+
+
+        public void SetNextSequenceIndex()
+        {
+            currentSequenceIndex++;
+            SequenceIndexUpdate?.Invoke(currentSequenceIndex);
+        }
+
+
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                SetNextSequenceIndex();
+            }
+        }
+
+    }
+}
